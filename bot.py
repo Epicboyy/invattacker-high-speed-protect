f85ab0c528c4b6bc9ba8c8d5002eb33e066e90d5
rootmelo92118 <32770959+rootmelo92118@users.noreply.github.com>
2018-05-24 16:51:14 +0800

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,28 @@
+from linepy import *
+
+
+client = LINE()
+client.log("Auth Token : " + str(client.authToken))
+
+oepoll = OEPoll(client)
+
+MySelf = client.getProfile()
+print("My MID : " + MySelf.mid)
+
+
+def NOTIFIED_INVITE_INTO_GROUP(op):
+    try:
+        client.rejectGroupInvitation(op.param1)
+    except Exception as e:
+        print(e)
+        print("\n\nNOTIFIED_INVITE_INTO_GROUP\n\n")
+        return
+      
+      
+oepoll.addOpInterruptWithDict({
+    OpType.NOTIFIED_INVITE_INTO_GROUP: NOTIFIED_INVITE_INTO_GROUP
+})
+
+
+while True:
+    oepoll.trace()
